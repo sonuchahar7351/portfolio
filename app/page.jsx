12b0875599ffef6de4c3395f277f684e10d6b1ae
sonuchahar7351 <sonuchahar7351@gmail.com>
2024-08-29 13:22:24 +0530

    portfolio tamplate

---
+++ app/page.jsx
@@ -0,0 +1,49 @@
+"use client";
+import Photo from "@/components/Photo";
+import Socials from "@/components/Socials";
+import Stats from "@/components/Stats";
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+
+const Home = () => {
+  return (
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:pt-4 xl:pb-24">
+          {/* text */}
+          <div className="text-center xl:text-left order-2 xl:order-none ">
+            <span className="text-xl">Software Developer</span>
+            <h1 className="h1 mb-4">
+              Hello I'm <br />{" "}
+              <span className="text-accent">Saunoo Chahar</span>
+            </h1>
+            <p className="max-w-[480px] mb-6 text-white/80">
+              I excel at crafting elegant digital experiences and i am
+              proficient i various programming languages and techologies.
+            </p>
+            <div className="flex flex-col xl:flex-row items-center gap-4">
+              <Button
+                variant="outline"
+                size="lg"
+                className="uppercase flex items-center gap-2"
+              >
+                <span>Download CV</span> <FiDownload className="text-xl " />
+              </Button>
+              <div className="mb-6 xl:mb-0">
+                 <Socials containerStyles="flex gap-4" iconStyles="w-8 h-8 border boder-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500"/>
+              </div>
+            </div>
+          </div>
+
+          {/* image */}
+          <div className="order-1 xl:order-none mb-8 xl:mb-0">
+            <Photo/>
+          </div>
+        </div>
+      </div>
+      <Stats/>
+    </section>
+  );
+};
+
+export default Home;
